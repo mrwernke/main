2eb72c8f68e28b52f617097de40d6e3ba42bf2dc
mrwernke <mwernke@ccs.k12.in.us>
2026-09-15 21:17:40 -0400

    Deploy app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? "/SATMathPrep/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
